ad8c3baaaf01ef783a8a34df90044f4e49919edd
Cybernetor066 <Cybernetortechnologies@gmail.com>
2021-10-09 23:20:39 +0100

    Second commit (Test IAC(2))

--- ssh_scripts.sh
+++ ssh_scripts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # # clone our repo to current workspace
-# git clone https://github.com/cybernetor066/symfony-cicd-project.git && cp -r */* . && rm -r symfony-cicd-project
+git config --global user.email "Cybernetortechnologies@gmail.com" && git config --global user.name "Cybernetor066"
+git init && git clone https://github.com/BrotherBrownNinja/forCICDpipeline/tree/main && cp -r */* . && rm -r main
 
 
+# To clone a specific branch:
+git init && git clone --branch main https://github.com/BrotherBrownNinja/forCICDpipeline.git && cp -r */* . && sudo rm -r forCICDpipeline
 
 
-
-
-
-
+sudo rm -r *
 
 
 
