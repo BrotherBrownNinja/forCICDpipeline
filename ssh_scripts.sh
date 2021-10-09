3c9f22b27ba7dd0ca235098513b4c97a89f0dd85
Cybernetor066 <Cybernetortechnologies@gmail.com>
2021-10-09 17:31:28 +0100

    First commit (Test IAC(1))

---
+++ ssh_scripts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# # clone our repo to current workspace
+# git clone https://github.com/cybernetor066/symfony-cicd-project.git && cp -r */* . && rm -r symfony-cicd-project
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
